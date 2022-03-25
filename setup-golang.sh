607b6bb90ff8a7d1f96e37c923a984017b28a219
DiscreteTom <discrete_tom@outlook.com>
2022-03-25 22:25:39 +0800

    add setup-golang.sh

---
+++ setup-golang.sh
@@ -0,0 +1,7 @@
+# install golang
+wget -q https://go.dev/dl/go1.18.linux-armv6l.tar.gz
+tar -C /usr/local/ -zxf go1.18.linux-armv6l.tar.gz
+echo 'PATH=$PATH:/usr/local/go/bin' >> /etc/profile
+
+# setup goproxy
+echo "export GOPROXY=https://goproxy.io,direct" >> /etc/profile
