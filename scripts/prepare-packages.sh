c6eba51f6c1d432baab1e6a35f5d2eab8f5d2ae5
DiscreteTom <discrete_tom@outlook.com>
2022-03-26 16:11:06 +0800

    fix prepare-packages.sh

--- scripts/prepare-packages.sh
+++ scripts/prepare-packages.sh
@@ -3,12 +3,13 @@ cp /etc/apt/sources.list /etc/apt/sources.list.backup
 cat > /etc/apt/sources.list << EOF
 deb [arch=armhf] http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ bullseye main non-free contrib rpi
 deb-src http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ bullseye main non-free contrib rpi
-deb [arch=arm64] http://mirrors.tuna.tsinghua.edu.cn/raspbian/multiarch/ bullseye main
 EOF
 cp /etc/apt/sources.list.d/raspi.list /etc/apt/sources.list.d/raspi.list.backup
 cat > /etc/apt/sources.list.d/raspi.list << EOF
 deb http://mirrors.tuna.tsinghua.edu.cn/raspberrypi/ bullseye main
 EOF
+
+# update package info
 apt update
 
 # install essential packages
